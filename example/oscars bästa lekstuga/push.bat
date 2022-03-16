5c9483b2c45e486f203c54d33830f19f3bb66e08
031simon <simon.k.hellsing@gmail.com>
2022-03-16 10:26:50 +0100

    test

--- example/oscars bästa lekstuga/push.bat
+++ example/oscars bästa lekstuga/push.bat
@@ -1,8 +1,8 @@
 set /p comment="Comment: "
 
-cd .\GICU-3
-git config --global user.email "example@gmail.com"
-git config --global user.name "example"
+cd C:\Users\Deltagare\Desktop\GICU-3
+git config --global user.email "simon.k.hellsing@gmail.com"
+git config --global user.name "031simon"
 git add -A
 git commit -a -m "%comment%"
 git push
