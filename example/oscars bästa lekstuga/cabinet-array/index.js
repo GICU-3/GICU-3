7e2ff0339d00e76b4559b28d8fde05556b532126
Kiwimagic03 <oscarmf03@gmail.com>
2022-03-21 15:46:08 +0100

    code, bom and hardware

---
+++ example/oscars bästa lekstuga/cabinet-array/index.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 600,
+        frame: false,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        resizable: false
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
