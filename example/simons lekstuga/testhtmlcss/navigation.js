6df560d7afa4727fe0af9f4df8571d2669dad597
031simon <simon.k.hellsing@gmail.com>
2022-04-13 14:27:11 +0200

    added navigation.js

---
+++ example/simons lekstuga/testhtmlcss/navigation.js
@@ -0,0 +1,4 @@
+function labbar() {
+    document.getElementById("searchbar").style.display = "none";
+    document.getElementById("bodySection").style.justifyContent = "right"; //kanske behöer ändras när divvar läggs till
+}
